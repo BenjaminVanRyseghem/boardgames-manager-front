8ee4c918d1e9a670717c8e2668043550b5b2f024
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2021-03-04 01:35:32 +0100

    Add user creation

--- src/pages/users/users.js
+++ src/pages/users/users.js
@@ -1,15 +1,46 @@
+// eslint-disable-line max-lines
 import "./users.scss";
-import { Col, Container, Row } from "reactstrap";
+import {
+	Button,
+	Col,
+	Container,
+	Form, FormFeedback,
+	FormGroup, Input,
+	Label,
+	Modal,
+	ModalBody,
+	ModalFooter,
+	ModalHeader,
+	Row
+} from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import i18n from "i18n/i18n";
 import info from "helpers/info";
-import { Link } from "react-router-dom";
 import Loading from "components/loading/loading";
 import Page from "../page";
 import PropTypes from "prop-types";
 import React from "react";
+import Select from "react-select";
 import Translate from "components/i18n/translate";
 import User from "models/user";
 import UserCard from "components/userCard/userCard";
+
+const defaultRole = {
+	id: "user",
+	name: "User"
+};
+
+const roles = [
+	{
+		id: "admin",
+		name: "Admin"
+	},
+	defaultRole,
+	{
+		id: "borrower",
+		name: "Borrower"
+	}
+];
 
 export class UsersContainer extends React.Component {
 	static defaultProps = {
@@ -20,22 +51,190 @@ export class UsersContainer extends React.Component {
 	static propTypes = {
 		currentUser: PropTypes.object.isRequired,
 		data: PropTypes.array,
-		error: PropTypes.object
+		dataMutate: PropTypes.func,
+		error: PropTypes.object,
+		onCreate: PropTypes.func.isRequired
+	};
+
+	state = {
+		open: false,
+		role: defaultRole,
+		firstName: "",
+		firstNameError: false,
+		lastName: "",
+		lastNameError: false,
+		email: "",
+		emailError: false,
+		password: "",
+		passwordError: false
 	};
 
 	componentDidUpdate(prevProps) {
 		if (prevProps.error !== this.props.error) {
 			info.error({
-				html: <Translate i18nKey="failedToLoadLocations">Failed to load users!</Translate>
+				html: <Translate i18nKey="failedToLoadUsers">Failed to load users!</Translate>
 			});
 		}
 	}
 
-	renderAddUser() {
+	toggleModal() {
+		this.setState((state) => ({
+			open: !state.open
+		}));
+	}
+
+	isValid() { // eslint-disable-line max-statements
+		let isValid = true;
+
+		if (this.state.email) {
+			this.setState({ emailError: false });
+		} else {
+			this.setState({ emailError: true });
+			isValid = false;
+		}
+
+		if (this.state.password) {
+			this.setState({ passwordError: false });
+		} else {
+			this.setState({ passwordError: true });
+			isValid = false;
+		}
+
+		if (this.state.firstName) {
+			this.setState({ firstNameError: false });
+		} else {
+			this.setState({ firstNameError: true });
+			isValid = false;
+		}
+
+		if (this.state.lastName) {
+			this.setState({ lastNameError: false });
+		} else {
+			this.setState({ lastNameError: true });
+			isValid = false;
+		}
+
+		return isValid;
+	}
+
+	onCreate() {
+		if (!this.isValid()) {
+			return;
+		}
+
+		this.setState({ open: false });
+
+		this.props.onCreate({
+			email: this.state.email,
+			firstName: this.state.firstName,
+			lastName: this.state.lastName,
+			password: this.state.password,
+			role: this.state.role.id
+		}, this.props.dataMutate);
+	}
+
+	renderAddUserModal() {
+		let toggle = this.toggleModal.bind(this);
+
 		return (
-			<Col key="add-user" className="card-holder" sm={4}>
-				<div className="userCard">
-					<Link className="add-user" to="/add-user">
+			<Modal className="add-user-modal" isOpen={this.state.open} toggle={toggle}>
+				<ModalHeader toggle={toggle}>
+					<Translate i18nKey="addAUser">Add a user</Translate>
+				</ModalHeader>
+				<ModalBody>
+					<Form>
+						<FormGroup>
+							<Label>
+								<Translate i18nKey="user.role">Role</Translate>
+							</Label>
+							<Select
+								className="locations-select"
+								formatOptionLabel={(option) => option.name}
+								getOptionLabel={(option) => option.name}
+								getOptionValue={(option) => option.id}
+								options={roles}
+								placeholder={i18n.t("placeholder", "Select...")}
+								value={this.state.role}
+								onChange={(role) => this.setState({ role })}
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Label>
+								<Translate i18nKey="user.email">Email</Translate>
+							</Label>
+							<Input
+								invalid={this.state.emailError}
+								value={this.state.email}
+								onChange={({ target: { value: email } }) => this.setState({ email })}
+							/>
+							<FormFeedback>
+								<Translate i18nKey="user.emailError">Empty email</Translate>
+							</FormFeedback>
+						</FormGroup>
+						<FormGroup>
+							<Label>
+								<Translate i18nKey="user.password">Password</Translate>
+							</Label>
+							<Input
+								invalid={this.state.passwordError}
+								type="password"
+								value={this.state.password}
+								onChange={({ target: { value: password } }) => this.setState({ password })}
+							/>
+							<FormFeedback>
+								<Translate i18nKey="user.passwordError">Empty password</Translate>
+							</FormFeedback>
+						</FormGroup>
+						<FormGroup>
+							<Label>
+								<Translate i18nKey="user.firstName">First name</Translate>
+							</Label>
+							<Input
+								invalid={this.state.firstNameError}
+								value={this.state.firstName}
+								onChange={({ target: { value: firstName } }) => this.setState({ firstName })}
+							/>
+							<FormFeedback>
+								<Translate i18nKey="user.firstNameError">Empty first name</Translate>
+							</FormFeedback>
+						</FormGroup>
+						<FormGroup>
+							<Label>
+								<Translate i18nKey="user.lastName">Last name</Translate>
+							</Label>
+							<Input
+								invalid={this.state.lastNameError}
+								value={this.state.lastName}
+								onChange={({ target: { value: lastName } }) => this.setState({ lastName })}
+							/>
+							<FormFeedback>
+								<Translate i18nKey="user.lastNameError">Empty last name</Translate>
+							</FormFeedback>
+						</FormGroup>
+					</Form>
+				</ModalBody>
+				<ModalFooter>
+					<Button color="primary" onClick={() => {
+						this.onCreate();
+					}}>
+						<Translate i18nKey="addAUser">Add a user</Translate>
+					</Button>{" "}
+					<Button color="secondary" onClick={toggle}><Translate i18nKey="cancel">Cancel</Translate></Button>
+				</ModalFooter>
+			</Modal>
+		);
+	}
+
+	renderAddUser() {
+		if (!this.props.currentUser.canAddUsers()) {
+			return null;
+		}
+
+		return (
+			<>
+				{this.renderAddUserModal()}
+				<Col key="add-user" className="card-holder" sm={4}>
+					<div className="userCard" onClick={this.toggleModal.bind(this)}>
 						<div className="content-wrapper">
 							<div className="content-inner-wrapper">
 								<div className="content">
@@ -48,9 +247,9 @@ export class UsersContainer extends React.Component {
 								</div>
 							</div>
 						</div>
-					</Link>
-				</div>
-			</Col>
+					</div>
+				</Col>
+			</>
 		);
 	}
 
@@ -101,11 +300,35 @@ export default class Users extends Page {
 
 	title = <Translate i18nKey="users">Users</Translate>;
 
+	onCreate(body, dataMutate) {
+		return this.fetch("/api/v1/user", {
+			method: "POST",
+			body
+		})
+			.then((data) => {
+				info.success({
+					title: <Translate i18nKey="info.success">Success</Translate>,
+					html: <Translate i18nKey="user.userCreatedSuccessfully">
+						{"User successfully created"}
+					</Translate>,
+					onAfterClose: () => dataMutate(data)
+				});
+			})
+			.catch(() => {
+				info.error({
+					html: <Translate i18nKey="user.failedToCreateUser">Failed to create user!</Translate>
+				});
+			});
+	}
+
 	renderContent() {
 		return (
 			<div className="usersContainer">
 				<this.swr model={User} url="/api/v1/user">
-					<UsersContainer currentUser={this.props.user}/>
+					<UsersContainer
+						currentUser={this.props.user}
+						onCreate={this.onCreate.bind(this)}
+					/>
 				</this.swr>
 			</div>
 		);
